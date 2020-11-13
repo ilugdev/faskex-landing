3daceba99f9ab0893df8f43370ee1b507cec31d8
Ilug <ilugdev@gmail.com>
2020-11-12 20:49:03 -0430

    responsive fixes

--- js/menu.js
+++ js/menu.js
@@ -4,24 +4,20 @@
 
 //MOSTRAR MENU
 
-$(document).ready(function(){
-  $("#btn_menu").click(function(){
-
-        $("#sideBar").css("display", "block");
-         $("#sideBar").animate({right: '0%'}, "slow");
-         $(".dark").css("display", "block");
-         $(".dark").animate({opacity: '1'});
-
+$(document).ready(function () {
+  $("#btn_menu").click(function () {
+    $("#sideBar").css("display", "block");
+    $("#sideBar").animate({ right: "0%" }, "slow");
+    $(".dark").css("display", "block");
+    $(".dark").animate({ opacity: "1" });
   });
 });
 
 //OCULTAR MENU
 
-$(document).ready(function(){
-  $("#menu_close").click(function(){
-        $("#sideBar").animate({right: '-100%'}, "slow");
-        $(".dark").css("display", "none");
-
-
+$(document).ready(function () {
+  $(".menu_close").on("click", function () {
+    $("#sideBar").animate({ right: "-100%" }, "slow");
+    $(".dark").css("display", "none");
   });
 });
